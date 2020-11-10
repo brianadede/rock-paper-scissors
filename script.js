c8b557598bb51f3cad72371775168c95b37ce497
braiadede <brianadede@gmail.com>
2020-11-10 20:13:29 +0300

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function games(){
+
     function computerPlay() {
         function getNumber(){
             return Math.floor(Math.random()*(3-1+1))+1;
@@ -11,49 +11,117 @@ function games(){
         else {return 'scissors'}
     }
 
-    const computerSelection = computerPlay();
     
     
-    const playerSelection = prompt('rock, paper, scissors?');
-      
+
         
+        const rockButton= document.querySelector('#rock');
+        const paperButton = document.querySelector('#paper');
+        const scissorsButton = document.querySelector('#scissors');
+        const restartButton = document.querySelector('#restart');
+        const currentRound=document.querySelector('#round');
+        
+
+        rockButton.addEventListener('click',() =>{
+            var playerSelection='rock';
+            var computerSelection = computerPlay();
+            compare(computerSelection,playerSelection);
+            scores();
     
+        })
+        paperButton.addEventListener('click',() =>{
+            playerSelection='paper'
+            computerSelection=computerPlay();
+            compare(computerSelection,playerSelection);
+            scores();
+        })
+        scissorsButton.addEventListener('click',() =>{
+            playerSelection= 'scissors';
+            computerSelection=computerPlay();
+            compare(computerSelection,playerSelection)
+            scores();
+        })
+        restartButton.addEventListener('click',() =>{
+            count1=0;
+            count2=0;
+            round=1;
+            currentRound.textContent= 'ROUND: 1'
+            scoreBoard.textContent='Player: 0 Computer: 0'
+            results.textContent='';
+            bestOfFive.textContent='';
+        })
+    
+    
+    var results= document.querySelector('#results');
+    var scoreBoard= document.querySelector('#scoreBoard');
+    var bestOfFive= document.querySelector('#bestOfFive'); 
+    var count1=0; 
+    var count2=0;
+    var round=1;
+    
+        
+    function compare(computerSelection,playerSelection){
         if (computerSelection === playerSelection){
-            console.log("it's a tie");}
+            results.textContent="it's a tie";
+        }
         if (playerSelection==='rock'){
             if (computerSelection==='paper'){
-                console.log('You lose! paper wins')
+                results.textContent= 'You lose! paper wins';
+                count2++;
             }
             else { 
                 if (computerSelection==='scissors'){
-                    console.log('You win! rock wins')
+                    results.textContent= 'You win! rock wins';
+                    count1++;
                 }
             }
         }
-        if (playerSelection==='paper'){
+        else if (playerSelection==='paper'){
             if (computerSelection==='rock'){
-                console.log('You win! paper wins')
+                results.textContent= 'You win! paper wins'
+                count1++;
             }
             else { 
                 if (computerSelection==='scissors'){
-                    console.log('You lose!scissors wins')
+                    results.textContent= 'You lose!scissors wins'
+                    count2++;
                 }
             }
         } 
-        if (playerSelection==='scissors'){
+        else if (playerSelection==='scissors'){
             if (computerSelection==='paper'){
-                console.log('You win! scissors wins')
+                results.textContent= 'You win! scissors wins'
+                count1++
             }
             else { 
                 if (computerSelection==='rock'){
-                    console.log('You lose! rock wins')
+                    results.textContent= 'You lose! rock wins'
+                    count2++
                 }
             }
-        }     
-}    
-
-function game(){
-    for (var i=0;i<5;i++){
-        games()
+        }
     }
-}
+
+    function scores(){
+        scoreBoard.textContent= 'Player: '+count1+' Computer: '+count2
+        
+        if (count1===5){
+            bestOfFive.textContent='You win'+' ROUND: '+round;
+            count1=0;
+            count2=0;
+            round++;
+            currentRound.textContent= 'ROUND: '+round;
+        }
+        else if (count2===5){
+            bestOfFive.textContent='Computer wins'+' ROUND: '+round;
+            count1=0;
+            count2=0;
+            round++;
+            currentRound.textContent= 'ROUND: '+round;
+        }
+
+    }
+    
+
+
+
